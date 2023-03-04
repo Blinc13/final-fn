3cad7dd435f2a951371bd01963823e2ee3fe8af5
Blinc <65546512+Blinc13@users.noreply.github.com>
2023-03-04 18:46:21 +0300

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,48 @@
+//! # Final_fn
+//! This crate provides ***final_fn*** macros, what executes given code when leaving code block
+//! # Example
+//! ```
+//! use final_fn::final_fn;
+//!
+//! fn main() {
+//!     final_fn!({
+//!         println!("End of main!");
+//!     });
+//!
+//!     println!("Hello world!");
+//! }
+//! ```
+
 #![no_std]
 
+/// # How this macros works
+/// This macros creates ***FinalFunc*** structure, what implements Drop trait
+/// # Examples
+/// This short examples show, how to use this macros!
+/// - **Code block**
+/// ```
+/// use final_fn::final_fn;
+///
+/// fn main() {
+///     final_fn!({
+///         println!("End of main!");
+///     });
+///
+///     println!("Hello world!");
+/// }
+/// ```
+/// - **Expression**
+/// ```
+/// use final_fn::final_fn;
+///
+/// fn main() {
+///     final_fn!(
+///         println!("End of main!")
+///     );
+///
+///     println!("Hello world!");
+/// }
+/// ```
 #[macro_export]
 macro_rules! final_fn {
     ($code:block) => {
